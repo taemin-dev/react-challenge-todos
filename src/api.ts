61ff91e59dc1ae62adda9fe7adcf4e28836cca55
ptm119 <133971709+taemin-dev@users.noreply.github.com>
2024-01-19 08:59:36 +0900

    Promise to async/await

--- src/api.ts
+++ src/api.ts
@@ -1,25 +1,19 @@
 const API_URL = "https://api.coinpaprika.com/v1";
 
-export function fetchCoins() {
-  return fetch(API_URL + "/coins")
-    .then((response) => response.json())
-    .then((json) => json.slice(0, 100));
+export async function fetchCoins() {
+  return await (await (await fetch(API_URL + "/coins")).json()).slice(0, 100);
 }
 
-export function fetchCoinInfo(coinId: string) {
-  return fetch(API_URL + "/coins/" + coinId).then((response) =>
-    response.json()
-  );
+export async function fetchCoinInfo(coinId: string) {
+  return await (await fetch(API_URL + "/coins/" + coinId)).json();
 }
 
-export function fetchCoinTickers(coinId: string) {
-  return fetch(API_URL + "/tickers/" + coinId).then((response) =>
-    response.json()
-  );
+export async function fetchCoinTickers(coinId: string) {
+  return await (await fetch(API_URL + "/tickers/" + coinId)).json();
 }
 
-export function fetchCoinHistory(coinId: string) {
-  return fetch(
-    `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
-  ).then((response) => response.json());
+export async function fetchCoinHistory(coinId: string) {
+  return await (
+    await fetch(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`)
+  ).json();
 }
